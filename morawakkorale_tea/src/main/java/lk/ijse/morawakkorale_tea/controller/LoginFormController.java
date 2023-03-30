3200214394297b82aae53d507df945c39afa8560
KasunKVI <kkhettiarachchi10@gmail.com>
2023-03-30 23:12:04 +0530

    Log in controller 2 nd

--- morawakkorale_tea/src/main/java/lk/ijse/morawakkorale_tea/controller/LoginFormController.java
+++ morawakkorale_tea/src/main/java/lk/ijse/morawakkorale_tea/controller/LoginFormController.java
@@ -1,22 +1,65 @@
 package lk.ijse.morawakkorale_tea.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginFormController {
 
+    @FXML
+    public TextField txtUserId;
+    public TextField txtPassword;
+
     public void LoginbtnOnAction() throws IOException {
 
-    Stage stage = new Stage();
-    Parent root = FXMLLoader.load(getClass().getResource("/view/collector_dashboard_form.fxml"));
-          stage.setTitle("Collector Dashboard");
-          stage.centerOnScreen();
-          stage.setScene(new Scene(root));
+        String id = txtUserId.getText();
+        String password = txtPassword.getText();
+        Stage stage = new Stage();
 
-          stage.show();
+        if (id.equals("A001") && password.equals("collector")){
+
+            Parent root = FXMLLoader.load(getClass().getResource("/view/collector_dashboard_form.fxml"));
+            stage.setTitle("Collector Dashboard");
+            stage.centerOnScreen();
+            stage.setScene(new Scene(root));
+
+            stage.show();
+
+        }else if (id.equals("A002") && password.equals("manager")){
+
+            Parent root = FXMLLoader.load(getClass().getResource("/view/manager_dashboard_form.fxml"));
+            stage.setTitle("Manager Dashboard");
+            stage.centerOnScreen();
+            stage.setScene(new Scene(root));
+
+            stage.show();
+
+        } else if (id.equals("A003") && password.equals("accountant")){
+
+            Parent root = FXMLLoader.load(getClass().getResource("/view/accountant_dashboard_form.fxml"));
+            stage.setTitle("Accountant Dashboard");
+            stage.centerOnScreen();
+            stage.setScene(new Scene(root));
+
+            stage.show();
+
+        } else if (id.equals("A004") && password.equals("g_m")){
+
+            Parent root = FXMLLoader.load(getClass().getResource("/view/general_manager_dashboard_form.fxml"));
+            stage.setTitle("General Manager Dashboard");
+            stage.centerOnScreen();
+            stage.setScene(new Scene(root));
+
+            stage.show();
+
+        }
+
+
+
     }
 }
---
+++ morawakkorale_tea/src/main/java/lk/ijse/morawakkorale_tea/dto/tm/User.java
@@ -0,0 +1,17 @@
+package lk.ijse.morawakkorale_tea.dto.tm;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class User {
+
+    private String user_id;
+    private String password;
+
+}

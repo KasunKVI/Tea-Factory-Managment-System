a21cc535019dba986743af1ce0f335b6259536c8
KasunKVI <kkhettiarachchi10@gmail.com>
2023-03-31 12:28:17 +0530

    manger dashboard pages

--- morawakkorale_tea/src/main/java/lk/ijse/morawakkorale_tea/controller/ManagerDashboardFormController.java
+++ morawakkorale_tea/src/main/java/lk/ijse/morawakkorale_tea/controller/ManagerDashboardFormController.java
@@ -1,13 +1,19 @@
 package lk.ijse.morawakkorale_tea.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 public class ManagerDashboardFormController  {
 
     @FXML
     public Pane menuBarPanel;
+    public AnchorPane backgroundPane;
 
     public void hideMenuBar(MouseEvent mouseEvent) {
 
@@ -27,4 +33,34 @@ public class ManagerDashboardFormController  {
     }
 
 
+    public void showSupplierManage(ActionEvent actionEvent) throws IOException {
+
+        backgroundPane.getChildren().clear();
+        backgroundPane.getChildren().add(FXMLLoader.load(getClass().getResource("/view/suppliers_manage_form.fxml")));
+
+    }
+
+    public void logout(ActionEvent actionEvent) {
+    }
+
+    public void showBuyersManage(ActionEvent actionEvent) throws IOException {
+
+        backgroundPane.getChildren().clear();
+        backgroundPane.getChildren().add(FXMLLoader.load(getClass().getResource("/view/customers_manage_form.fxml")));
+
+    }
+
+    public void showTransportersManage(ActionEvent actionEvent) throws IOException {
+
+        backgroundPane.getChildren().clear();
+        backgroundPane.getChildren().add(FXMLLoader.load(getClass().getResource("/view/transporters_manage_form.fxml")));
+
+    }
+
+    public void showProductManage(ActionEvent actionEvent) throws IOException {
+
+        backgroundPane.getChildren().clear();
+        backgroundPane.getChildren().add(FXMLLoader.load(getClass().getResource("/view/product_manage_form.fxml")));
+
+    }
 }
